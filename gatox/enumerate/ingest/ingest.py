1ec4ca1763f35a8921a35063d2090284e30aeedb
AdnaneKhan <adnanekhan@users.noreply.github.com>
2024-09-16 18:26:47 -0400

    Clean up ingestor docs and and unit tests.

--- gatox/enumerate/ingest/ingest.py
+++ gatox/enumerate/ingest/ingest.py
@@ -2,10 +2,7 @@ import time
 import random
 import threading
 from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import wait, as_completed
-
-
-from queue import Queue
+from concurrent.futures import as_completed
 
 from gatox.caching.cache_manager import CacheManager
 from gatox.models.workflow import Workflow
@@ -24,6 +21,13 @@ class DataIngestor:
     @classmethod
     def update_count(cls, batch: int):
         """
+        Update the class-level counter if the provided batch number is greater than the current counter.
+    
+        Args:
+            batch (int): The batch number to compare with the current counter.
+    
+        Returns:
+            None
         """
         with cls.__lock:
             if batch > cls.__counter:
@@ -31,29 +35,44 @@ class DataIngestor:
 
     @classmethod
     def check_status(cls):
-        """
+        """ This method returns the value of the class variable __counter.
         """
         return cls.__counter
 
     @classmethod
     def perform_parallel_repo_ingest(cls, api, org, repo_count):
-        """Perform a parallel query of repositories up to the count
-        within a given organization.
+        """
+        Perform a parallel query of repositories up to the count within a given organization.
+    
+        Args:
+            api (object): The API client used to make the GET requests.
+            org (str): The organization name to query repositories from.
+            repo_count (int): The number of repositories to query.
+    
+        Returns:
+            list: A list of repositories retrieved from the organization.
         """
         repos = []
-
+    
         def make_query(increment):
-            """Makes query to retrieve repos for given page.
-            Attempts up to 5 times.
+            """
+            Makes a query to retrieve repositories for a given page.
+            Attempts up to 5 times if the request fails.
+    
+            Args:
+                increment (int): The page number to query.
+    
+            Returns:
+                list: A list of repositories for the given page, or None if the query fails.
             """
             get_params = {
                 "type": "public",
                 "per_page": 100,
                 "page": increment
             }
-
+    
             sleep_timer = 4
-            for i in range (0, 5):
+            for _ in range(0, 5):
                 repos = api.call_get(f'/orgs/{org}/repos', params=get_params)
                 if repos.status_code == 200:
                     return repos.json()
@@ -61,9 +80,9 @@ class DataIngestor:
                     time.sleep(sleep_timer)
                     sleep_timer = sleep_timer * 2
             Output.error("Unable to query. Will miss repositories.")
-
+    
         batches = (repo_count // 100) + 1
-
+    
         with ThreadPoolExecutor(max_workers=10) as executor:
             futures = []
             for batch in range(1, batches + 1):
@@ -72,23 +91,33 @@ class DataIngestor:
                 listing = future.result()
                 if listing:
                     repos.extend([repo for repo in listing if not repo['archived']])
-
+    
         return repos
 
     @classmethod
     def perform_query(cls, api, work_query, batch):
-        """Performs GraphQL query of repositories. Gato-X will use 3
-        attempts, increasing the sleep timer from 4, 8, and then finally 16 
-        seconds.
+        """
+        Performs a GraphQL query of repositories with up to 3 attempts, increasing the sleep timer from 4, 8, and then finally 16 seconds.
+    
+        Args:
+            api (object): The API client used to make the POST requests.
+            work_query (dict): The GraphQL query to be executed.
+            batch (int): The batch number for which the query is being performed.
+    
+        Returns:
+            list or dict: The nodes or values from the query result if successful, otherwise None.
+    
+        Raises:
+            Exception: If an error occurs during the query execution.
         """
         try:
-            for i in range (0, 4):
+            for _ in range(0, 4):
                 # We lock if another thread is sleeping due to a rate limit
                 # but if no other thread is sleeping, we want to move on
                 # to query.
                 while cls.__rl_lock.locked():
                     time.sleep(0.1)
-
+    
                 result = api.call_post('/graphql', work_query)
                 # Sometimes we don't get a 200, fall back in this case.
                 if result.status_code == 200:
@@ -98,14 +127,13 @@ class DataIngestor:
                         return result.json()['data']['nodes']
                     else:
                         return result.json()['data'].values()
-                    break
                 elif result.status_code == 403:
                     with cls.__rl_lock:
-                        time.sleep(15 + random.randint(0,3))
+                        time.sleep(15 + random.randint(0, 3))
                 else:
                     # Add some jitter
-                    time.sleep(10 + random.randint(0,3))
-           
+                    time.sleep(10 + random.randint(0, 3))
+    
             Output.warn(f"GraphQL attempts failed for batch {str(batch)}, will revert to REST for impacted repos.")
         except Exception as e:
             Output.warn(
@@ -116,35 +144,36 @@ class DataIngestor:
 
     @staticmethod
     def construct_workflow_cache(yml_results):
-        """Creates a cache of workflow yml files retrieved from graphQL. Since
-        graphql and REST do not have parity, we still need to use rest for most
-        enumeration calls. This method saves off all yml files, so during org
-        level enumeration if we perform yml enumeration the cached file is used
-        instead of making github REST requests. 
-
+        """
+        Creates a cache of workflow YAML files retrieved from GraphQL. Since GraphQL and REST do not have parity,
+        REST is still used for most enumeration calls. This method saves all YAML files, so during organization-level
+        enumeration, if YAML enumeration is performed, the cached file is used instead of making GitHub REST requests.
+    
         Args:
-            yml_results (list): List of results from individual GraphQL queries
-            (100 nodes at a time).
+            yml_results (list): List of results from individual GraphQL queries (100 nodes at a time).
+    
+        Returns:
+            None
         """
         if yml_results is None:
             return
-
+    
         cache = CacheManager()
         for result in yml_results:
             # If we get any malformed/missing data just skip it and 
             # Gato will fall back to the contents API for these few cases.
             if not result:
                 continue
-                
+    
             if 'nameWithOwner' not in result:
                 continue
-
+    
             owner = result['nameWithOwner']
             cache.set_empty(owner)
-            # Empty means no yamls, so just skip.
+            # Empty means no YAMLs, so just skip.
             if result['object']:
                 for yml_node in result['object']['entries']:
-                    yml_name = yml_node['name']      
+                    yml_name = yml_node['name']
                     if yml_node['type'] == 'blob' and \
                       (yml_name.lower().endswith('yml') \
                       or yml_name.lower().endswith('yaml')): 
@@ -152,8 +181,8 @@ class DataIngestor:
                             contents = yml_node['object']['text']
                             wf_wrapper = Workflow(owner, contents, yml_name)
                             
-                            cache.set_workflow(owner, yml_name, wf_wrapper) 
-
+                            cache.set_workflow(owner, yml_name, wf_wrapper)
+    
             repo_data = {
                 'full_name': result['nameWithOwner'],
                 'html_url': result['url'],
@@ -180,11 +209,11 @@ class DataIngestor:
                 'allow_forking': result['forkingAllowed'],
                 'environments': []
             }
-
+    
             if 'environments' in result and result['environments']:
                 # Capture environments not named github-pages
-                envs = [env['node']['name']  for env in result['environments']['edges'] if env['node']['name'] != 'github-pages']
+                envs = [env['node']['name'] for env in result['environments']['edges'] if env['node']['name'] != 'github-pages']
                 repo_data['environments'] = envs
-                    
+    
             repo_wrapper = Repository(repo_data)
             cache.set_repository(repo_wrapper)
---
+++ unit_test/test_ingest.py
@@ -0,0 +1,65 @@
+import unittest
+from unittest.mock import MagicMock
+from gatox.enumerate.ingest.ingest import DataIngestor
+from gatox.caching.cache_manager import CacheManager
+
+class TestConstructWorkflowCache(unittest.TestCase):
+
+    def setUp(self):
+        self.workflow = MagicMock()
+        self.repository = MagicMock()
+        CacheManager._instance = None
+        self.cache_manager = CacheManager()
+        Workflow = MagicMock(return_value=self.workflow)
+        Repository = MagicMock(return_value=self.repository)
+        self.yml_results = [
+            {
+                'nameWithOwner': 'owner/repo1',
+                'object': {
+                    'entries': [
+                        {
+                            'name': 'workflow1.yml',
+                            'type': 'blob',
+                            'object': {'text': 'content1'}
+                        }
+                    ]
+                },
+                'url': 'http://example.com/repo1',
+                'isPrivate': False,
+                'defaultBranchRef': {'name': 'main'},
+                'isFork': False,
+                'stargazers': {'totalCount': 10},
+                'pushedAt': '2023-01-01T00:00:00Z',
+                'viewerPermission': 'ADMIN',
+                'isArchived': False,
+                'forkingAllowed': True,
+                'environments': {'edges': []}
+            }
+        ]
+
+    def test_construct_workflow_cache_none(self):
+        # Test with None input
+        assert DataIngestor.construct_workflow_cache(None) is None
+
+    def test_construct_workflow_cache_empty_list(self):
+        # Test with empty list input
+        DataIngestor.construct_workflow_cache([])
+
+        assert CacheManager()._instance.repo_wf_lookup == {}
+        assert CacheManager()._instance.workflow_cache == {}
+        
+    def test_construct_workflow_cache_valid_input(self):
+        # Test with valid input
+        DataIngestor.construct_workflow_cache(self.yml_results)
+
+        assert CacheManager().is_repo_cached('owner/repo1') is True
+
+    def test_construct_workflow_cache_malformed_data(self):
+        # Test with malformed data
+        malformed_data = [{'invalid_key': 'invalid_value'}]
+        DataIngestor.construct_workflow_cache(malformed_data)
+
+        assert not CacheManager().is_repo_cached('invalid_key')
+
+if __name__ == '__main__':
+    unittest.main()
