06a794078561499dd9830497a00afda80f8da103
Adnan Khan <AdnaneKhan@users.noreply.github.com>
2024-12-14 17:05:03 -0500

    Add error handling to address #62. (#65)

--- gatox/cli/cli.py
+++ gatox/cli/cli.py
@@ -337,8 +337,14 @@ def enumerate(args, parser):
     exec_wrapper.add_organizations(orgs)
     exec_wrapper.add_repositories(repos)
 
-    if args.output_json:
-        Output.write_json(exec_wrapper, args.output_json)
+    try:
+
+        if args.output_json:
+            Output.write_json(exec_wrapper, args.output_json)
+    except Exception as output_error:
+        Output.error(
+            "Encountered an error writing the output JSON, this is likely a Gato-X bug."
+        )
 
 
 def search(args, parser):
