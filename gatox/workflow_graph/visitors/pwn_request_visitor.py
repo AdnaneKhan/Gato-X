7c1be96fb0c3e9f7733812e8d52cf7003d467b60
AdnaneKhan <adnanekhan@users.noreply.github.com>
2025-01-03 14:18:50 -0800

    fix..

--- gatox/workflow_graph/visitors/pwn_request_visitor.py
+++ gatox/workflow_graph/visitors/pwn_request_visitor.py
@@ -1,8 +1,12 @@
+from gatox.workflow_graph.graph.tagged_graph import TaggedGraph
+from gatox.workflow_graph.visitors.visitor_utils import VisitorUtils
+from gatox.github.api import Api
+from gatox.workflow_parser.utility import CONTEXT_REGEX
+from gatox.caching.cache_manager import CacheManager
+
+
 class PwnRequestVisitor:
-    """
-    Visitor class responsible for identifying and processing potential security issues
-    related to Pwn (Power and Privilege Escalation) requests within workflow graphs.
-    """
+    """Visits the graph to find potential Pwn Requests."""
 
     @staticmethod
     def _process_single_path(path, graph, api, rule_cache, results):
@@ -10,32 +14,19 @@ class PwnRequestVisitor:
         Process a single path for potential security issues.
 
         This method analyzes a given path within the workflow graph to identify and flag
-        potential security vulnerabilities. It inspects each node for specific tags,
-        checks deployment environment rules, and determines if approval gates are required.
-
-        The processing involves:
-        1. Initializing lookup dictionaries and an approval gate flag.
-        2. Iterating through each node in the provided path.
-        3. For nodes tagged as "JobNode":
-            a. Checks deployment environment rules by retrieving or caching
-               environment protection rules from the API.
-            b. Processes each deployment to determine if it requires an approval gate.
-            c. Updates the `approval_gate` flag based on the evaluation of deployment rules.
-        4. Continues processing subsequent nodes based on the updated state.
+        potential security vulnerabilities related to Pwn (Privilege Escalation) requests.
+        It inspects each node for specific tags, evaluates deployment environment rules,
+        and determines if approval gates are required based on the analysis.
 
         Args:
             path (List[Node]):
                 The sequence of nodes representing a potential security path to process.
-
             graph (TaggedGraph):
                 The workflow graph containing all nodes and their relationships.
-
             api (Api):
                 An instance of the API wrapper to interact with external services.
-
             rule_cache (dict):
                 A cache storing environment protection rules for repositories to avoid redundant API calls.
-
             results (dict):
                 A dictionary aggregating the detected security issues, organized by repository.
 
@@ -54,7 +45,6 @@ class PwnRequestVisitor:
             tags = node.get_tags()
 
             if "JobNode" in tags:
-
                 # Check deployment environment rules
                 if node.deployments:
                     if node.repo_name in rule_cache:
@@ -75,3 +65,159 @@ class PwnRequestVisitor:
                         if deployment in rules:
                             approval_gate = True
                             continue
+
+                paths = graph.dfs_to_tag(node, "permission_blocker", api)
+                if paths:
+                    break
+
+                paths = graph.dfs_to_tag(node, "permission_check", api)
+                if paths:
+                    approval_gate = True
+
+                if node.outputs:
+                    for o_key, val in node.outputs.items():
+                        if "env." in val and val not in env_lookup:
+                            for key in env_lookup.keys():
+                                if key in val:
+                                    flexible_lookup[o_key] = env_lookup[key]
+
+            elif "StepNode" in tags:
+                if node.is_checkout:
+                    # Terminal
+                    checkout_ref = node.metadata
+                    if "inputs." in node.metadata:
+                        processed_var = VisitorUtils.process_context_var(node.metadata)
+                        if processed_var in env_lookup:
+                            original_val = env_lookup[processed_var]
+                            checkout_ref = original_val
+                        elif processed_var in input_lookup:
+                            checkout_ref = input_lookup[processed_var]
+
+                    elif "env." in node.metadata:
+                        for key, val in env_lookup.items():
+                            if key in node.metadata:
+                                checkout_ref = val
+                                break
+
+                    if (
+                        approval_gate
+                        and VisitorUtils.check_mutable_ref(
+                            checkout_ref, path[0].get_tags()
+                        )
+                    ) or not approval_gate:
+                        sinks = graph.dfs_to_tag(node, "sink", api)
+                        if sinks:
+                            VisitorUtils.append_path(path, sinks[0])
+
+                        VisitorUtils._add_results(path, results)
+
+                if node.outputs:
+                    for key, val in node.outputs.items():
+                        if "env." in val:
+                            pass
+
+                if node.hard_gate:
+                    break
+
+                if node.soft_gate:
+                    approval_gate = True
+
+            elif "WorkflowNode" in tags:
+                if index != 0 and "JobNode" in path[index - 1].get_tags():
+                    # Caller job node
+                    node_params = path[index - 1].params
+                    # Set lookup for input params
+                    input_lookup.update(node_params)
+                if index == 0:
+                    repo = CacheManager().get_repository(node.repo_name)
+                    if repo.is_fork():
+                        break
+
+                    if "pull_request_target:labeled" in tags:
+                        approval_gate = True
+
+                    # Check workflow environment variables.
+                    env_vars = node.get_env_vars()
+                    for key, val in env_vars.items():
+                        if isinstance(val, str) and "github." in val:
+                            env_lookup[key] = val
+
+            elif "ActionNode" in tags:
+                VisitorUtils.initialize_action_node(graph, api, node)
+
+    @staticmethod
+    def _finalize_result():
+        """
+        Takes a known reachable checkout and attempts to find an associated sink.
+
+        This method finalizes the result by identifying sinks related to a reachable checkout.
+        It ensures that any identified sinks are properly linked and recorded in the results.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: Indicates that the method is not yet implemented.
+        """
+        raise NotImplementedError("This method needs to be implemented.")
+
+    @staticmethod
+    def find_pwn_requests(graph: TaggedGraph, api: Api, ignore_workflow_run=False):
+        """
+        Identify and process potential Pwn Requests within the workflow graph.
+
+        This method searches for paths within the workflow graph that could lead to
+        privilege escalation vulnerabilities. It starts by querying nodes with specific
+        tags related to pull requests and issues, then performs depth-first searches to
+        locate checkout nodes, and processes each discovered path for potential security issues.
+
+        Args:
+            graph (TaggedGraph):
+                The workflow graph containing all nodes and their relationships.
+            api (Api):
+                An instance of the API wrapper to interact with GitHub APIs.
+            ignore_workflow_run (bool, optional):
+                Determines whether to ignore nodes tagged with "workflow_run". Defaults to False.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        query_taglist = [
+            "issue_comment",
+            "pull_request_target",
+            "pull_request_target:labeled",
+        ]
+
+        if not ignore_workflow_run:
+            query_taglist.append("workflow_run")
+
+        # Retrieve all repository-related nodes with the specified tags
+        nodes = graph.get_nodes_for_tags(query_taglist)
+
+        all_paths = []
+
+        for cn in nodes:
+            paths = graph.dfs_to_tag(cn, "checkout", api)
+            if paths:
+                all_paths.append(paths)
+
+        results = {}
+        rule_cache = {}
+
+        for path_set in all_paths:
+            for path in path_set:
+                try:
+                    PwnRequestVisitor._process_single_path(
+                        path, graph, api, rule_cache, results
+                    )
+                # TODO: Make this more granular once all edge cases are handled.
+                except Exception as e:
+                    print(f"Error processing path: {e}")
+        print("PWN:")
+        VisitorUtils.ascii_render(results)
